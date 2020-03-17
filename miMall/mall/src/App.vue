252f84dadb2a4c4566115782f50dced6960edb3d
zch1999 <1901394767@qq.com>
2020-03-17 23:36:35 +0800

    环境变量设置

--- miMall/mall/src/App.vue
+++ miMall/mall/src/App.vue
@@ -5,11 +5,16 @@
 </template>
 
 <script>
-
+import storage from './storage'
 export default {
   name: 'App',
   components: {
   },
+  mounted(){
+    // storage.setItem('a',1)
+    // storage.setItem('abc',{a:1},'user')
+    storage.clear('a')
+  }
 }
 </script>
 
