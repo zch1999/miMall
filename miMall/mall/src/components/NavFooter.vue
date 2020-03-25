d065fa15c61f8d49bcaa6abd33e40ca7195f31d7
zch1999 <1901394767@qq.com>
2020-03-25 22:48:34 +0800

    nav-footer组件完成

--- miMall/mall/src/components/NavFooter.vue
+++ miMall/mall/src/components/NavFooter.vue
@@ -1,6 +1,19 @@
 <!--  -->
 <template>
-  <div></div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="" >
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="">个人主页</a><span>|</span>
+      <a href="">小米之家</a><span>|</span>
+      <a href="">米聊</a><span>|</span>
+      <a href="">小米天猫店</a><span>|</span>
+      <a href="">小爱开放平台</a><span>|</span>
+    </div>
+    <div class="copyright">Copyright @2019 <span class="domain"> mi.futurefe.com </span> All Right Reserved</div>
+  </div>
 </template>
 
 <script>
@@ -13,6 +26,37 @@ export default {
 }
 </script>
 
-<style lang='stylus' scoped>
-
+<style lang='scss' scoped>
+  .footer{
+    height: 234px;
+    border-top: 4px solid #FF6600;
+    background-color: #333333;
+    color: #999999;
+    font-size: 16px;
+    text-align: center;
+    .footer-logo{
+      margin-top: 46px;
+      margin-bottom: 31px;
+      img{
+        width: 53px;
+        height: 36px;
+        margin-bottom: 13px;
+      }
+    }
+    .footer-link{
+      a{
+        color: #999999;
+        display: inline-block;
+      }
+      span{
+        margin: 0 10px;
+      }
+      margin-bottom: 10px;
+    }
+    .copyright{
+      .domain{
+        color: #FF6600;
+      }
+    }
+  }
 </style>
