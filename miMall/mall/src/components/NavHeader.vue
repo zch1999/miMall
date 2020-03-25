329e58c4b00f7d40b977c926be7628fc4134114f
zch1999 <1901394767@qq.com>
2020-03-25 23:45:53 +0800

    servicebar组件

--- miMall/mall/src/components/NavHeader.vue
+++ miMall/mall/src/components/NavHeader.vue
@@ -269,7 +269,7 @@ export default {
                 box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
                 z-index: 10;
                 transition: height 0.2s;
-                // background-color: #ffffff;
+                background-color: #ffffff;
                 .product{
                   position: relative;
                   float: left;
